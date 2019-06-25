1dfc5ae013226a745583adced9070bb853d6d4ec
w18oon <49589301+w18oon@users.noreply.github.com>
2019-06-25 19:59:07 +0700

    w18oon chat bot

--- index.php
+++ index.php
@@ -5,22 +5,77 @@ require_once 'vendor/autoload.php';
 $channelAccessToken = 'FHSGmy/OcieflUXOhLmeKbUWEJl9/47QdZjAg7Sybl2sVnDsSYlERzQ3jU557TZviiJO0nyrXhVHoyNt2Zfst1RQM6XghyY2ZZFmfzqj6eTVXitYhcGj9ndij03yh3wBZqW5rNcYpVlQhN9oOlgZtAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '68cf1b782ae45b6b0847ce8b9c0ae331';
 
-$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-
-foreach ($events as $event) 
-{
-    // $response = $bot->replyMessage($event->getReplyToken(), $outputText);
-    $response = $bot->replyText($event->getReplyToken(), 'hello!');
-    if ($response->isSucceeded())
-    {
-        echo 'Succeeded!';
-        return;
-    }
+/*Get Data From POST Http Request*/
+$datas = file_get_contents('php://input');
     
-    // Failed
-    echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+/*Decode Json From LINE Data Body*/
+$deCode = json_decode($datas,true);
+
+file_put_contents('log.txt', file_get_contents('php://input') . PHP_EOL, FILE_APPEND);
+
+$replyToken = $deCode['events'][0]['replyToken'];
+
+$messages = [];
+$messages['replyToken'] = $replyToken;
+$messages['messages'][0] = getFormatTextMessage("...");
+// $messages['messages'][0] = $deCode['events'][0]['message']['text'];
+
+$encodeJson = json_encode($messages);
+
+$LINEDatas['url'] = "https://api.line.me/v2/bot/message/reply";
+$LINEDatas['token'] = "FHSGmy/OcieflUXOhLmeKbUWEJl9/47QdZjAg7Sybl2sVnDsSYlERzQ3jU557TZviiJO0nyrXhVHoyNt2Zfst1RQM6XghyY2ZZFmfzqj6eTVXitYhcGj9ndij03yh3wBZqW5rNcYpVlQhN9oOlgZtAdB04t89/1O/w1cDnyilFU=";
+
+  $results = sentMessage($encodeJson,$LINEDatas);
+
+/*Return HTTP Request 200*/
+http_response_code(200);
+
+function getFormatTextMessage($text)
+{
+    $datas = [];
+    $datas['type'] = 'text';
+    $datas['text'] = $text;
+
+    return $datas;
 }
 
-http_response_code(200);
+function sentMessage($encodeJson,$datas)
+{
+    $datasReturn = [];
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $datas['url'],
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS => $encodeJson,
+      CURLOPT_HTTPHEADER => array(
+        "authorization: Bearer ".$datas['token'],
+        "cache-control: no-cache",
+        "content-type: application/json; charset=UTF-8",
+      ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+
+    curl_close($curl);
+
+    if ($err) {
+        $datasReturn['result'] = 'E';
+        $datasReturn['message'] = $err;
+    } else {
+        if($response == "{}"){
+        $datasReturn['result'] = 'S';
+        $datasReturn['message'] = 'Success';
+        }else{
+        $datasReturn['result'] = 'E';
+        $datasReturn['message'] = $response;
+        }
+    }
+
+    return $datasReturn;
+}
