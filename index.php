11ecfdb3a1c2003b9e78c3d402d69df87625583d
w18oon <49589301+w18oon@users.noreply.github.com>
2019-06-25 19:56:16 +0700

    w18oon chat bot

--- index.php
+++ index.php
@@ -5,34 +5,22 @@ require_once 'vendor/autoload.php';
 $channelAccessToken = 'FHSGmy/OcieflUXOhLmeKbUWEJl9/47QdZjAg7Sybl2sVnDsSYlERzQ3jU557TZviiJO0nyrXhVHoyNt2Zfst1RQM6XghyY2ZZFmfzqj6eTVXitYhcGj9ndij03yh3wBZqW5rNcYpVlQhN9oOlgZtAdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '68cf1b782ae45b6b0847ce8b9c0ae331';
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 
-$events = file_get_contents('php://input');
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
-foreach ($events as $event)
+foreach ($events as $event) 
 {
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText());
-    $response = $bot->replyMessage($event->getReplyToken(), $textMessageBuilder);
+    // $response = $bot->replyMessage($event->getReplyToken(), $outputText);
+    $response = $bot->replyText($event->getReplyToken(), 'hello!');
     if ($response->isSucceeded())
     {
-        http_response_code(200);
+        echo 'Succeeded!';
         return;
     }
-
+    
+    // Failed
     echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 }
-
-// $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-// $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
-
-// foreach ($events as $event) {
-// 	// Message Event = TextMessage
-// 	if (($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
-//         $messageText = strtolower(trim($event->getText()));
-//         $outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("text message");
-// 		$response = $bot->replyMessage($event->getReplyToken(), $outputText);
-// 	}
-// }
 
 http_response_code(200);
